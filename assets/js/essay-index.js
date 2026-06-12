1c62c0757d2bab55afe07c85835e56ec8257a4bd
KernelGreyBeret <tcburke314@gmail.com>
2026-06-11 22:17:43 -0400

    Create essay-index.js

---
+++ assets/js/essay-index.js
@@ -0,0 +1,14 @@
+window.AP_ESSAYS = [
+  {
+    title: "The Three A’s of Zero Trust",
+    url: "/essays/the-three-as-of-zero-trust.html"
+  },
+  {
+    title: "The End State Is the Requirement",
+    url: "/essays/the-end-state-is-the-requirement.html"
+  },
+  {
+    title: "The Checkbox Fallacy",
+    url: "/essays/the-checkbox-fallacy.html"
+  }
+];
